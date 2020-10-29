a77ee64fb5ad3e1c3277f4d51b3e6a29f3381e4f
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2020-10-29 14:25:39 -0400

    add coordination tranration

--- cbf_ros/scripts/cbf_controller.py
+++ cbf_ros/scripts/cbf_controller.py
@@ -4,6 +4,12 @@
 #
 # If start in manual
 # $ rosrun cbf_ros cbf_controller.py
+#
+# About coordination
+# HSR is handled by \odom coordination and Gazebo is handled by \map coordination 
+# \odom: That is robots global position. Usually, the center of coodination means when robot is started. The coordination will have localizaion error and updated by any loclaiozer frequently.
+# \map: That is map's center, like center of Earth. Generally robot will not start from the center. 
+# \base_foot_print: the robot center coordination. We can build controller based on this coordinaiton. 
 
 import rospy
 import sys
@@ -11,6 +17,7 @@ import argparse
 
 # ROS msg
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Vector3
 from nav_msgs.msg import Odometry
 from gazebo_msgs.msg import ModelState 
@@ -43,13 +50,35 @@ class CBF_CONTROLLER(object):
                 rospy.wait_for_service ('/gazebo/get_model_state')
                 self.get_model_srv = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
                 self.tOdometry_subscriber = rospy.Subscriber('/hsrb/odom_ground_truth', Odometry, self.tOdometry_callback, queue_size=10)
-                self.odometry = Odometry()
+                self.tOdometry = Odometry()
+                self.odometry_subscriber = rospy.Subscriber('/global_pose', PoseStamped, self.odometry_callback, queue_size=10)
+                self.poseStamped = PoseStamped()
+
+                # listener of tf.
+                self.tfListener = tf.TransformListener()
 
         def __del__(self):
                 pass
 
         def tOdometry_callback(self, odometry):
-                self.odometry = odometry
+                self.odometry = odometry # this odometry's coodination is \map
+
+        def odometry_callback(self, poseStamped):
+                self.poseStamped = poseStamped
+        
+        def gazebo_pos_transformPose(self, frame_id, gazebo_pose):
+                gazebo_pose_temp = PoseStamped()
+                gazebo_pose_temp.header = gazebo_pose.header
+                gazebo_pose_temp.header.frame_id = 'map'
+                gazebo_pose_temp.pose = gazebo_pose.pose
+                while not rospy.is_shutdown():
+                        try:
+                                gazebo_pos_trans = self.tfListener.transformPose(frame_id, gazebo_pose_temp)
+                                break
+                        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                                continue
+
+                return gazebo_pos_trans
 
         def controller_loop_callback(self, event):
                 # this controller loop call back.
@@ -60,27 +89,33 @@ class CBF_CONTROLLER(object):
                         rospy.loginfo('Current time %i %i', now.secs, now.nsecs)
 
                         # true odom
-                        tTwist = self.odometry.twist.twist
-                        rospy.loginfo('tOdometry \n %s', tTwist)
+                        rospy.loginfo('tOdometry\n %s', self.odometry)
+
+                        # odom
+                        #rospy.loginfo('odometry \n %s', self.poseStamped)
+
+                # covert todometry 
 
                 # get human model state from Gazebo
                 model_actor1 = GetModelStateRequest()
                 model_actor1.model_name = 'actor1'
-                actor1 = self.get_model_srv(model_actor1)
-                angular1 = orientation2angular(actor1.pose.orientation)
+                actor1 = self.get_model_srv(model_actor1) # the pose date is based on /map
+                actor1base_footprint_pose = self.gazebo_pos_transformPose('base_footprint', actor1) # trasfer /map->/base_footprint
+                angular1 = orientation2angular(actor1base_footprint_pose.pose.orientation)      # transfer orientaton(quaternion)->agular(euler)
                 if DEBUG:
-                        rospy.loginfo('actor1\nposition:\n%s\nangular:\n%s', actor1.pose.position, angular1)
-                
+                        rospy.loginfo('actor1 in base_footprint\nposition:\n%s\nangular:\n%s', actor1base_footprint_pose.pose.position, angular1)
+
                 model_actor2 = GetModelStateRequest()
                 model_actor2.model_name = 'actor2'
-                actor2 = self.get_model_srv(model_actor2)
-                angular2 = orientation2angular(actor2.pose.orientation)
+                actor2 = self.get_model_srv(model_actor2) # the pose date is based on /map
+                actor2base_footprint_pose = self.gazebo_pos_transformPose('base_footprint', actor2) # trasfer /map->/base_footprint
+                angular2 = orientation2angular(actor2base_footprint_pose.pose.orientation)      # transfer orientaton(quaternion)->agular(euler)
                 if DEBUG:
-                        rospy.loginfo('actor2\nposition:\n%s\nangular:\n%s', actor2.pose.position, angular2)
+                        rospy.loginfo('actor2 in base_footprint\nposition:\n%s\nangular:\n%s', actor2base_footprint_pose.pose.position, angular2)
 
                 # making vw data and publish it.
                 vel_msg = Twist()
-                vel_msg.linear.x  = 0.0 #[m/sec]
+                vel_msg.linear.x  = 0.3 #[m/sec]
                 vel_msg.angular.z = 0.0 #[rad/sec]
                 self.vw_publisher.publish(vel_msg)
 

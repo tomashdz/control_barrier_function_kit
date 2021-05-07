a3de845b302fe9ca0ac3ecf4c392ef6634487c0c
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2021-05-07 15:26:00 -0400

    add arguments to agent.py

--- cbf_ros/scripts/agent.py
+++ cbf_ros/scripts/agent.py
@@ -1,22 +1,24 @@
 #! /usr/bin/env python
+import argparse
+
 import rospy
 
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Header
-from gazebo_msgs.msg import ModelState 
+from gazebo_msgs.msg import ModelState
 from gazebo_msgs.srv import GetModelState, GetModelStateRequest, SetModelState
 
 freq = 10 #Hz
 speed = -0.2 #m/s
 
 class Agent(object):
-    def __init__(self):
+    def __init__(self, model_name):
         rospy.wait_for_service ('/gazebo/get_model_state')
         self.get_model_srv = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
         self.set_model_srv = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
 
         self.model = GetModelStateRequest()
-        self.model.model_name='agent'
+        self.model.model_name = model_name
 
     def control_callback(self, event):
         result = self.get_model_srv(self.model)
@@ -32,9 +34,13 @@ class Agent(object):
 
 if __name__ == '__main__':
     # Process arguments
+    p = argparse.ArgumentParser(description='agent node')
+    p.add_argument('--model_name', nargs=1, type=str, required=True, help='the taregt model name on gazebo')
+    args = p.parse_args(rospy.myargv()[1:])
+
     try:
-        rospy.init_node('agent')
-        agent = Agent()
+        rospy.init_node(args.model_name[0]+'_controller')
+        agent = Agent(args.model_name[0])
         rospy.Timer(rospy.Duration(1.0/freq), agent.control_callback)
         rospy.spin()
 

2590941298f4eec6c05bd1c9499cf7a9b75d5dc9
Bardh Hoxha <bardhh@asu.edu>
2021-05-10 13:09:07 -0400

    oop basics

--- src/runScript.py
+++ src/runScript.py
@@ -1,8 +1,25 @@
 from sympy import symbols, lambdify, diff
-from system import System
+from system import *
 
 xr_0, xr_1 = symbols('xr_0 xr_1')
-states=(xr_0, xr_1)
+xo_0, xo_1 = symbols('xo_0 xo_1')
 
-sys1 = System(states)
-print(sys1.states)
+ego = System('ego',(xr_0, xr_1))
+agent1 = System('agent1',(xo_0, xo_1))
+
+# print(ego.system_details())
+# print(agent1.system_details())
+
+# xr_2 = symbols('xr_2')
+# ego.add_state((xr_2,))
+# print(ego.system_details())
+
+# System.set_solver('z')
+# print(ego.system_details())
+# print(agent1.system_details())
+
+# print(System.add_numbers([1,2]))
+
+# agent_2_deter = Deterministic('agent2_deter',(xo_0, xo_1), [1, 1], [2, 2])
+# print(agent_2_deter.system_details())
+
--- src/system.py
+++ src/system.py
@@ -3,5 +3,32 @@ import numpy as np
 
 class System(object):
 
-    def __init__(self, states):
+    solver = 'ode45'
+
+    def __init__(self, name, states):
+        self.name = name
         self.states =  states
+        self.type = type
+
+    #@property
+    def system_details(self):
+        return '{} {} {}'.format(self.name, self.states, self.solver)
+
+    def add_state(self, new_state):
+        self.states += new_state
+
+    @classmethod
+    def set_solver(cls, solver):
+        cls.solver = solver
+
+    @staticmethod
+    def add_numbers(num_list):
+        return sum(num_list)
+
+class Deterministic(System):
+    def __init__(self, name, states, C, D):
+        super().__init__(name, states)
+        self.C = C
+        self.D = D
+    def system_details(self):
+        return '{} {} {} {} {}'.format(self.name, self.states, self.solver, self.C, self.D)
